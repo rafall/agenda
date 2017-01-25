73909ddf30ccf1ed3b4ed433ce9948d885796d56
Rafael Hiroki <rafael.hiroki@gmail.com>
2017-01-25 14:31:19 -0200

    Comunicação feita através do HTTPS

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var app = require('./server/routes');
+var fs = require('fs');
+var https = require('https');
 
-app.listen(8000, function() {
-	console.log('Listening on port 8000');
+app.all('*', function(request, response, next) {
+    if(request.secure) {
+        return next();
+    }
+    response.redirect('https://localhost:3443/');
+});
+
+var secureServer = https.createServer( {
+    key: fs.readFileSync('keys/private.key'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}, app);
+
+secureServer.listen(3443, function() {
+    console.log('Secure server listening on port 3443');
 });
